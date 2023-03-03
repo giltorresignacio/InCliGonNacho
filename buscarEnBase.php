55d76af141d35a4397e0a3a7a0c3addebb96b1d4
Ignacio Gil <75325299+giltorresignacio@users.noreply.github.com>
2023-03-03 14:07:53 +0100

    registrouwu

--- buscarEnBase.php
+++ buscarEnBase.php
@@ -1,34 +1,58 @@
 <?php
-$mailInicioSesion = $_GET['emailInicioSesion'];
-$contraInicioSesion = $_GET['contrasenhaInicioSesion'];
+if (isset($_GET['emailInicioSesion'])) {
 
-/* Conectar a la BD y luego ya actuo siempre sobre la variable conexion*/
-$conexion = mysqli_connect("localhost", "root", "", "escuela");
+    $mailInicioSesion = $_GET['emailInicioSesion'];
+    $contraInicioSesion = $_GET['contrasenhaInicioSesion'];
 
-/* Para seleccionar la bd*/
-mysqli_select_db($conexion, "escuela") or die("No se puede seleccionar la BD");
+    /* Conectar a la BD y luego ya actuo siempre sobre la variable conexion*/
+    $conexion = mysqli_connect("localhost", "root", "", "escuela");
 
-/* Lazo la consulta sobre la BD*/
-$usuario = mysqli_query($conexion, "select mail, contrasena, admin from profesores where mail='$mailInicioSesion' AND contrasena='$contraInicioSesion'");
+    /* Para seleccionar la bd*/
+    mysqli_select_db($conexion, "escuela") or die("No se puede seleccionar la BD");
 
-/* para detectar errores*/
-if (mysqli_connect_errno()) {
-    printf("<p>Conexión fallida: %s</p>", mysqli_connect_error());
-    exit();
-}
+    /* Lazo la consulta sobre la BD*/
+    $usuario = mysqli_query($conexion, "select mail, contrasena, admin from profesores where mail='$mailInicioSesion' AND contrasena='$contraInicioSesion'");
 
-/* Devuelve el número de filas del resultado */
-$numr = mysqli_num_rows($usuario);
-if ($numr > 0) {
-    for ($i = 0; $i < $numr; $i++) {
-        /* El resultado es realmente una matriz y voy cogiendo por filas con esa función*/
-        $fila = mysqli_fetch_array($usuario, MYSQLI_ASSOC);
-        /* Uso un foreach para recorrer fila ya que es una array .*/
-        foreach ($fila as $key => $value) {
-            $arrayParaJson[$key][] = $value;
-        }
+    /* para detectar errores*/
+    if (mysqli_connect_errno()) {
+        printf("<p>Conexión fallida: %s</p>", mysqli_connect_error());
+        exit();
     }
-    echo json_encode($arrayParaJson);
+
+    /* Devuelve el número de filas del resultado */
+    $numr = mysqli_num_rows($usuario);
+    if ($numr > 0) {
+        for ($i = 0; $i < $numr; $i++) {
+            /* El resultado es realmente una matriz y voy cogiendo por filas con esa función*/
+            $fila = mysqli_fetch_array($usuario, MYSQLI_ASSOC);
+            /* Uso un foreach para recorrer fila ya que es una array .*/
+            foreach ($fila as $key => $value) {
+                $arrayParaJson[$key][] = $value;
+            }
+        }
+        echo json_encode($arrayParaJson);
+    } else {
+        return false;
+    }
 } else {
-    return false;
+
+    $nomReg = $_GET['nomReg'];
+    $epellidoReg = $_GET['apell'];
+    $usuReg = $_GET['usuReg'];
+    $emailReg = $_GET['emailReg'];
+    $passReg = $_GET['passReg'];
+    $departReg = $_GET['depart'];
+    /* Conectar a la BD y luego ya actuo siempre sobre la variable conexion*/
+    $conexion = mysqli_connect("localhost", "root", "", "escuela");
+
+    /* Para seleccionar la bd*/
+    mysqli_select_db($conexion, "escuela") or die("No se puede seleccionar la BD");
+    echo "INSERT INTO `profesores`(`nombre`, `apellidos`, `departamento`, 
+    `usuario`, `contrasena`, `mail`, `admin`) VALUES ('$nomReg', '$epellidoReg' ,'$departReg','$usuReg','$passReg','$emailReg',0)";
+    /* Lazo la consulta sobre la BD*/
+    $insert = mysqli_query($conexion, "INSERT INTO `profesores`(`nombre`, `apellidos`, `departamento`, 
+    `usuario`, `contrasena`, `mail`, `admin`) VALUES ('$nomReg', '$epellidoReg' ,'$departReg','$usuReg','$passReg','$emailReg',0)");
+    echo $insert;
+    /* para detectar errores*/
+    
 }
--- registro.js
+++ registro.js
@@ -12,47 +12,62 @@ var regContra = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{6,15}$/
 var upperCase = new RegExp('[A-Z]');
 var lowerCase = new RegExp('[a-z]');
 var numbers = new RegExp('[0-9]');
+var comNom = 0;
+var comApe = 0;
+var comDepar = 0;
+var comUsu = 0;
+var comEmail = 0;
+var comPass = 0;
 
 function enviarValidarPeticionAJAX(event) {
-    if (user.value != '' && rgex.test(user.value) && password.value != '') {
+    console.log("uwu");
+    console.log(comApe);
+    console.log(comNom);
+    console.log(comDepar);
+    console.log(comUsu);
+    console.log(comEmail);
+    console.log(comPass);
+    if (comNom == 1 && comApe == 1 && comDepar == 1 & comUsu == 1 && comEmail == 1 && comPass == 1) {
         console.log("validacionesok")
         xhr = new XMLHttpRequest();
         xhr.addEventListener('readystatechange', gestionarRespuesta, false);
-        console.log("buscarEnBase.php?emailInicioSesion=" + user.value + "&contrasenhaInicioSesion=" + password.value);
-        xhr.open("GET", "buscarEnBase.php?emailInicioSesion=" + user.value + "&contrasenhaInicioSesion=" + password.value, false);
+        console.log("buscarEnBase.php?nomReg=" + nomReg.val() + "&apell=" + apell.val() + "&usuReg=" + usuReg.val() + "&emailReg=" + emailReg.val() + "&passReg=" + passReg.val() + "&depart=" + depart.val());
+        xhr.open("GET", "buscarEnBase.php?nomReg=" + nomReg.val() + "&apell="
+            + apell.val() + "&usuReg=" + usuReg.val() + "&emailReg=" + emailReg.val()
+            + "&passReg=" + passReg.val() + "&depart=" + depart.val(), false);
         xhr.send();
     } else {
-        document.getElementById('chiquito').style.display = "block";
+        window.alert("Error");
         event.preventDefault();
     }
 }
 function gestionarRespuesta(evento) {
     if (evento.target.readyState == 4 && evento.target.status == 200) {
         if (xhr.response == false) {
-            document.getElementById('chiquito').style.display = "block";
+            window.alert("Usuario ya existente")
             formulario.preventDefault();
         } else {
-            datos = JSON.parse(xhr.response);
-            sessionStorage.setItem("email", datos.mail);
-            sessionStorage.setItem("contrasena", datos.contrasena);
-            sessionStorage.setItem("admin", datos.admin);
-            salidas = datos.getElementsByTagName("salida");
+            window.alert("Usuario creado correctamente")
         }
     }
 }
 
 function validarNombre() {
     if (nomReg.val() != '' && regNombreApe.test(nomReg.val())) {
+        comNom = 1;
         $('#chiquitoNombre').css('display', 'none');
     } else {
+        comNom = 0;
         $('#chiquitoNombre').css('display', 'block');
     }
 }
 
 function validarApellido() {
     if (apell.val() != '' && regNombreApe.test(apell.val())) {
+        comApe = 1;
         $('#chiquitoApellido').css('display', 'none');
     } else {
+        comApe = 0;
         $('#chiquitoApellido').css('display', 'block');
 
     }
@@ -60,8 +75,10 @@ function validarApellido() {
 
 function validarDepartamento() {
     if (depart.val() != '') {
+        comDepar = 1;
         $('#chiquitoDepartamento').css('display', 'none');
     } else {
+        comDepar = 0;
         $('#chiquitoDepartamento').css('display', 'block');
 
     }
@@ -69,16 +86,20 @@ function validarDepartamento() {
 
 function validarUsuario() {
     if (usuReg.val() != '' && regNombreApe.test(usuReg.val())) {
+        comUsu = 1;
         $('#chiquitoUsuario').css('display', 'none');
     } else {
+        comUsu = 0;
         $('#chiquitoUsuario').css('display', 'block');
     }
 }
 
 function validarEmail() {
     if (emailReg.val() != '' && rgex.test(emailReg.val())) {
+        comEmail = 1;
         $('#chiquitoCorreo').css('display', 'none');
     } else {
+        comEmail = 0;
         $('#chiquitoCorreo').css('display', 'block');
     }
 }
@@ -86,8 +107,10 @@ function validarEmail() {
 function validarPassword() {
 
     if (passReg.val() != '' && regContra.test(passReg.val())) {
+        comPass = 1;
         $('#tamanho').css('color', 'green');
     } else {
+        comPass = 0;
         $('#tamanho').css('color', 'red');
         //MAYUS
         if (passReg.val().match(upperCase)) {
