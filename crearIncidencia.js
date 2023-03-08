c624c81f8921b8b6231dc7454649bf2ea6942199
Ignacio Gil <75325299+giltorresignacio@users.noreply.github.com>
2023-03-08 13:00:11 +0100

    z<

--- crearIncidencia.js
+++ crearIncidencia.js
@@ -5,24 +5,22 @@ xhr = new XMLHttpRequest();
 function enviarValidarPeticionAJAX() {
     tabla = document.getElementById('tabla');
     xhr.addEventListener('readystatechange', gestionarRespuesta, false);
-    xhr.open("GET", "buscarEnBase.php?idBuscarIncidencias=" + sessionStorage.getItem("id"), false);
+    xhr.open("GET", "buscarEnBase.php?claveDeCrearIncidencias=1", false);
     xhr.send();
 }
+
 function gestionarRespuesta() {
     if (xhr.readyState == 4 && xhr.status == 200) {
         datos = JSON.parse(xhr.response);
-
         for (index = 0; index < (datos.Tipo).length; index++) {
             console.log(datos.estado[index]);
             if (datos.estado[index] == 'creada') {
-                console.log("Por que");
                 color = 'red'
             } else if (datos.estado[index] == 'en curso') {
                 color = 'yellow'
             } else {
                 color = 'green'
             }
-            console.log(color);
             tabla.innerHTML += "<tr>" +
                 "<td>" + datos.Tipo[index] + "</td>" +
                 "<td>" + datos.Aula[index] + "</td>" +
